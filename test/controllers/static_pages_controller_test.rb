e00ec4d1244295eaf472bd3f9dbfba4ea097cbec
cleeharris <clee_harris@hotmail.com>
2016-02-07 13:38:37 -0700

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
    assert_response :success
     assert_select "title","About | RedOwl Systems"
   end
+
+  test "should get contact" do
+    get :contact
+   assert_response :success
+    assert_select "title","Contact | RedOwl Systems"
+  end
 end
--- test/controllers/static_pages_controller_test.rb~
+++ test/controllers/static_pages_controller_test.rb~
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home | RedOwl Systems"
+    assert_select "title","RedOwl Systems"
   end
 
   test "should get help" do
